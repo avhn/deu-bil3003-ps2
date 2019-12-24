f137d8c1a36dc60052ee8900d19ade5af277becf
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-12-24 07:56:02 +0300

    Add tests for CartNode

--- test.py
+++ test.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import random
 
 from cart import utils
 from cart import parse
-from cart import node
+from cart.node import CartNode
 
 
 class UtilTests(unittest.TestCase):
@@ -28,8 +29,49 @@ class ParseTests(unittest.TestCase):
             assert D and len(D) > 1 and len(D.pop()) > 1
 
 
-class NodeTests(unittest.TestCase):
-    pass
+class CartNodeTests(unittest.TestCase):
+    D = list(parse.parse_set())
+
+    def test_init(self):
+        """Also returns the node created."""
+
+        sample_set = random.sample(CartNodeTests.D, len(CartNodeTests.D) // 3)
+        node = CartNode(sample_set)
+        assert node.left is None and node.right is None
+        return node
+
+    def test_set_branches(self):
+        """Also returns the node created."""
+
+        node = self.test_init()
+        node_right = self.test_init()
+        node.set_branches(None, node_right)
+        assert node.left is None and node.right is node_right
+        return node
+
+    def test_set_as_leaf(self):
+        """Also returns the node created."""
+
+        node = self.test_set_branches()
+        # node is subtree
+        value = 'test value'
+        node.set_as_leaf(value)
+        # node is value node
+        assert node.value and node.value is value
+        assert node.left is None and node.right is None
+        return node
+
+    def test_is_leaf(self):
+        assert self.test_set_branches().is_leaf() is False
+        assert self.test_set_as_leaf().is_leaf()
+
+    def test_is_node_valid(self):
+        empty_node = self.test_init()
+        assert empty_node.is_node_valid() is False
+        node = self.test_set_branches()
+        assert node.is_node_valid() is True
+        node.value = 'some val'
+        assert node.is_node_valid() is False
 
 
 if __name__ == '__main__':
