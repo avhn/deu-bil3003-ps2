74a416ce36a51a1296a4967eddbdb0069d0cac0c
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-12-26 06:09:49 +0300

    Add specific split generators

--- cart/utils.py
+++ cart/utils.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
+from itertools import chain
+from itertools import combinations
+import math
 
 
 def is_convertible(val: str):
-    return False not in [s.isnumeric() for s in val.split('.')]
+    """Test if string is convertible to a number."""
+
+    split = [s.isnumeric() for s in val.split('.')]
+    return 0 < len(split) < 3 and False not in split
 
 
 def normalize(val: str):
@@ -33,3 +39,36 @@ def gini_index(records):
     for count in counts.values():
         result -= (count / len(records)) ** 2
     return result
+
+
+def generate_splits_numbered(column: int, records):
+    distinct = set()
+    for record in records:
+        distinct.add(float(record[column]))
+    sorted_distinct = list(distinct)
+    sorted_distinct.sort()
+    for split_point in sorted_distinct:
+        yield split_point
+        # yield lambda l: l <= split_point, lambda r: split_point < r
+
+
+def generate_splits_string(column: int, records):
+    distinct = set()
+    for record in records:
+        distinct.add(str(record[column]))
+
+    duplicates = set()
+    print(distinct)
+    max_len_combination = math.ceil(len(distinct) / 2)
+    for left_branch in chain([combinations(distinct, l) for l in range(1, max_len_combination)]):
+        # use frozenset to be able to hash sets to detect duplicates
+        left_branch = frozenset(left_branch)
+        right_branch = frozenset(distinct.difference(left_branch))
+        duplicates.add(right_branch)
+        duplicates.add(left_branch)
+        if left_branch not in duplicates:
+            yield left_branch, right_branch
+
+
+def generate_splits(records):
+    pass
